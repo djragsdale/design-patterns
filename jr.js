ee7a51fb569780fdf0f5b07d63842d3201ee8c92
David Ragsdale <me@davidjragsdale.com>
2017-04-29 14:27:40 -0400

    Mark strings separately from other data types in JR.log.

--- jr.js
+++ jr.js
@@ -9,6 +9,9 @@ const JR = (function() {
 	}; // Custom utilities package
 	JR.utilities.isUndefined = obj => typeof obj === 'undefined';
 	JR.utilities.isNull = obj => obj === null;
+	JR.utilities.isString = str => typeof str === 'string';
+	JR.utilities.isObject = obj => ((null !== obj) && Object.prototype.toString.call(obj) === '[object Object]');
+	JR.utilities.isArray = arr => ((null !== arr) && Object.prototype.toString.call(arr) === '[object Array]');
 
 	const { isNull, isUndefined } = JR.utilities;
 
@@ -20,8 +23,22 @@ const JR = (function() {
 		// noColor: true
 	};
 
+	function markStrings(obj) {
+		if (JR.utilities.isString(obj)) {
+			return `"${obj}"`;
+		} else if (JR.utilities.isArray(obj)) {
+			return Array.prototype.map.call(obj, markStrings);
+		} else if (JR.utilities.isObject(obj)) {
+			for (let prop in obj) {
+				obj[prop] = markStrings(obj[prop]);
+			}
+		}
+
+		return obj;
+	}
+
 	JR.log = (...args) => {
-		console.log.apply(null, args.map(arg => prettyjson.render(isUndefined(arg) ? 'undefined' : isNull(arg) ? 'null' : arg, jsonOptions)));
+		console.log.apply(null, args.map(arg => prettyjson.render(isUndefined(arg) ? 'undefined' : isNull(arg) ? 'null' : markStrings(arg), jsonOptions)));
 	};
 
 	JR.sectionLog = name => console.log(`\n${prettyjson.render(`========== ${name} ==========`, { stringColor: 'magenta' })}`);
