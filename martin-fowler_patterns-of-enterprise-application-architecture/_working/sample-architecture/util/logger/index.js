a5c71a4b5881b600329a63f2ca51ec6f21605aab
David Ragsdale <me@davidjragsdale.com>
2020-05-19 20:39:10 -0400

    Add comments to logger

--- martin-fowler_patterns-of-enterprise-application-architecture/_working/sample-architecture/util/logger/index.js
+++ martin-fowler_patterns-of-enterprise-application-architecture/_working/sample-architecture/util/logger/index.js
@@ -10,18 +10,23 @@ function logForLevel(level, ...args) {
 }
 
 export default {
+  // Use for when you want extra debugging
   debug(...args) {
     logForLevel(2, ...args);
   },
+  // Use for formatted errors
   error(...args) {
     logForLevel(0, '[ERROR]', ...args);
   },
+  // Use for things you usually want logged
   info(...args) {
     logForLevel(1, ...args);
   },
+  // Use for messages you always want logged
   log(...args) {
     logForLevel(0, ...args);
   },
+  // Use for entering and exiting functions. Useful for when you want to trace non-errors
   trace(...args) {
     logForLevel(3, ...args);
   },
