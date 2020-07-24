d5156d6feb03b3e895afb99553fc7e4b2fca0414
David Ragsdale <me@davidjragsdale.com>
2020-07-24 07:05:50 -0400

    Add Mediator intent

--- behavioral/mediator.js
+++ behavioral/mediator.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// Intent:
+// Define an object that encapsulates how a set of objects interact. Mediator promotes loose
+// coupling by keeping objects from referring to each other explicitly, and it lets you vary their
+// interaction independently.
+
 function Widget () {
 
 	let _director;
